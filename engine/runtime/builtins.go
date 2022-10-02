400aeaf915972077fdb19a6c4be3ccc5bbd5023f
DevDane <dane@danecwalker.com>
2022-10-03 04:50:29 +1000

    read user input added

--- engine/runtime/builtins.go
+++ engine/runtime/builtins.go
@@ -7,6 +7,7 @@ package runtime
 
 import (
 	"fmt"
+	"strconv"
 
 	"github.com/danecwalker/ponic/engine/object"
 )
@@ -14,12 +15,44 @@ import (
 type Builtin = func(args ...object.Object) object.Object
 
 var Builtins = map[string]Builtin{
-	"print": func(args ...object.Object) object.Object {
-		_args := make([]interface{}, len(args))
-		for i, arg := range args {
-			_args[i] = unescape(arg.Inspect())
+	"print": _print,
+
+	"scan": _scan,
+
+	"int": _int,
+}
+
+func _print(args ...object.Object) object.Object {
+	_args := make([]interface{}, len(args))
+	for i, arg := range args {
+		_args[i] = unescape(arg.Inspect())
+	}
+	fmt.Print(_args...)
+	return nil
+}
+
+func _scan(args ...object.Object) object.Object {
+	var input string
+	_print(args...)
+	fmt.Scan(&input)
+	return &object.String{Value: input}
+}
+
+func _int(args ...object.Object) object.Object {
+	if len(args) != 1 {
+		panic("wrong number of arguments.")
+	}
+
+	switch arg := args[0].(type) {
+	case *object.Integer:
+		return arg
+	case *object.String:
+		i, err := strconv.ParseInt(arg.Value, 10, 64)
+		if err != nil {
+			panic(err)
 		}
-		fmt.Print(_args...)
-		return nil
-	},
+		return &object.Integer{Value: i}
+	default:
+		panic("argument to `int` not supported.")
+	}
 }
